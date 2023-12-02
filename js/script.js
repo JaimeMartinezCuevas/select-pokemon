a0cc014275d9b4679c33096531cbb14fe526af7b
JaimeMartinezCuevas <150029776+JaimeMartinezCuevas@users.noreply.github.com>
2023-12-02 15:42:42 +0100

    entrega

--- js/script.js
+++ js/script.js
@@ -1,41 +1,136 @@
-const pokemonSelect = document.getElementById("pokemon-select");
-const getPokemon = document.getElementById("get-pokemon");
+const searchBtn = document.getElementById('searchBtn');
+const resetBtn = document.getElementById('resetBtn');
+const prevBtn = document.getElementById('prevBtn');
+const resenextBtntBtn = document.getElementById('nextBtn');
 
-let callPokemon = () => {
-    const selectedPokemon = pokemonSelect.value;
+searchBtn.addEventListener('click', () => {searchPokemon();});
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Error al obtener datos del Pokémon');
-            }
-            return response.json();
-        })
-        .then((data) => {
-            getPokemon.innerHTML = '';
-            const pokemonInfo = document.createElement('div');
+resetBtn.addEventListener('click', () => {
+    app.innerHTML = '';
+    getPokemon();
+});
 
-            pokemonInfo.innerHTML = 
-            
-            `
-                <img src="${data.sprites.front_default}" alt="${data.name}"/>
-                <p>Nombre: <span>${data.name}</span></p>
-                <p>Tipo: <span>${data.types.map(type => type.type.name).join(', ')}</span></p>
-                <p>Altura: <span>${data.height}</span></p>
-                <p>Peso: <span>${data.weight}</span></p>
-            `;
 
-            getPokemon.appendChild(pokemonInfo);
-        })
-        .catch(error => {
-            console.error('Error:', error.message);
-        });
+
+// Paginación de la pokedex
+let currentPage = 1;
+
+// Máximo de pokemon que se mostrarán por página. Sustituye a ?limit=10 en el fetch de getPokemon()
+const maxPokemon = 10;
+
+//Los botones para moverse entre páginas usan la variable current page para aumentar o disminuir el valor
+prevBtn.addEventListener('click', () => {
+    //Con el >1 nos aseguramos de que no se pase de rosca
+    if (currentPage > 1) {
+        currentPage--;
+        getPokemon(currentPage);
+    }
+});
+
+nextBtn.addEventListener('click', () => {
+    currentPage++;
+    getPokemon(currentPage);
+});
+
+
+// Función para buscar el pokemon
+const searchPokemon = async () => {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+
+    try {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=2000");
+
+        if (!res.ok) {
+            throw new Error("Ha habido un error al cargar la información del pokemon");
+        }
+
+        const catchPokemon = await res.json();
+
+        // Filtrar la lista de Pokémon basándose en el nombre de búsqueda
+        const filteredPokemonList = catchPokemon.results.filter(pokemon => pokemon.name.includes(searchInput));
+
+        if (filteredPokemonList.length === 0) {
+            app.innerHTML = "No se han encontrado pokémon con ese nombre";
+        } else {
+
+            // Llama a la función printPokemon para imprimir los nombres y las imágenes de los pokemon filtrados
+            app.innerHTML = printPoke(filteredPokemonList);
+        }
+    } catch (error) {
+        console.error(error);
+        app.innerHTML = "Ha habido un error al cargar la información del pokemon";
+    }
 };
 
-pokemonSelect.addEventListener('change', () => {
-    callPokemon();
-});
+// Función para imprimir los pokemon en el HTML
+const printPoke = async (pokemonList) => {
 
-getPokemon.addEventListener('click', () => {
-    callPokemon();
-});
+    //Se me generaba una entrada extraña de la pokedex que imprimía [object-promise] al buscar un pokemon
+    //así que declaré una variable (entriesHTML) para que las entradas no se imprimieran directamente en el div app
+    //según he estado investigando, ocurre porque el bucle for no es asíncrono. He intentado enmendarlo pero no he sido capaz del todo porque me generama otros errores en la consola
+    //Parece que al encerrar los resultados en una variable lo maneja mejor y todo a la vez, pero bueno, si ye quien a afayase, yo me doy con un regodón en los dientes
+    let entriesHTML = '';
+
+    for (const poke of pokemonList) {
+        try {
+            const res = await fetch(poke.url);
+            if (!res.ok) {
+                throw new Error(`Hubo un error al cargar los datos de ${poke.name}`);
+            }
+            const pokemonData = await res.json();
+
+            const imageUrl = pokemonData.sprites.other['official-artwork'].front_default;
+            console.log(`Nombre: ${poke.name}, URL de la imagen: ${imageUrl}`);
+
+            let tipos = pokemonData.types.map((type) => {
+                return `<p class='${type.type.name} tipo'>${type.type.name}</p>`;
+            });
+            
+            tipos = tipos.join('');
+            console.log(`Nombre: ${poke.name}, tipos: ${tipos}`);
+
+
+            // app.innerHTML +=
+            entriesHTML += `
+                <div class='entry'>
+                    <img src="${imageUrl}" alt="Sprite pokemon"/>
+                    <p>${poke.name}</p>
+                    <div class='tipos'>${tipos}</div>
+                </div>
+            `;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // Actualizamos el contenido del elemento app después de construir todas las entradas
+    app.innerHTML = entriesHTML;
+};
+
+const getPokemon = async (page) => {
+    
+    try {
+
+        //he declarado una variable para cambiar la página y que se actualice según se mueven las páginas
+        const cambioPagina = (page - 1) * maxPokemon;
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${maxPokemon}&offset=${cambioPagina}`);
+
+        // const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+
+        if (!res.ok) {
+            throw new Error("Hubo un error al cargar los pokemon");
+        }
+        const catchPokemon = await res.json();
+        console.log(catchPokemon)
+
+        // Llama a la función printPokemon para imprimir los nombres y las imágenes de los pokemon
+        printPoke(catchPokemon.results);
+
+    } catch (error) {
+        console.error(error);
+        app.innerHTML = "Hubo un error al cargar los pokemon";
+    }
+};
+
+//Encendemos el programa
+getPokemon(currentPage);
